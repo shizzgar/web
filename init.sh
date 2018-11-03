a5657308caf48a52b433c68b77db2e1e73faac67
shizzgar <39241841+shizzgar@users.noreply.github.com>
2018-11-03 15:28:51 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
