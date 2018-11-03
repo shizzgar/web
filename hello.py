2f5b3da4daf3ce5100da2a6fa66eb70fe622d3ec
shizzgar <steeji.rat@gmail.com>
2018-11-03 14:33:21 +0300

    Gunicorn conf

---
+++ hello.py
@@ -0,0 +1,21 @@
+
+
+
+def hello(environ, start_response):
+
+
+    d = environ['QUERY_STRING']
+    d.pop(0,1)
+    d.split("&")
+    for i in d:
+        a += str(i) + '\n'
+
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return a    
+
+    
