1981b1e2651571d0276fc8d1b56a1bb6adcb6f45
shizzgar <steeji.rat@gmail.com>
2018-11-07 23:20:20 +0300

    Modified project for step 2.1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.test, name='test'),
+    url(r'^login/$', views.test, name='test'),
+    url(r'^signup/$', views.test, name='test'),
+    url(r'^ask/$', views.test, name='test'),
+    url(r'^popular/$', views.test, name='test'),
+    url(r'^new/$', views.test, name='test'),
+    url(r'^question/(?P<id>\d+)/$', views.test, name='test'),
 ]
--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+
