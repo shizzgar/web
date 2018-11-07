ec5ed84db93ed444672ee89d78a6ffbbc8fe54fb
shizzgar <steeji.rat@gmail.com>
2018-11-07 23:25:49 +0300

    init.sh now is working

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo gunicorn -c /home/box/web/etc/hello.py hello:hello
+sudo gunicorn -c /home/box/web/etc/django.py ask.wsgi:application
 
