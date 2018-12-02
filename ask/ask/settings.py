4578ea8efabc5aecde4895ae8bf9de82c71fda1a
shizzgar <steeji.rat@gmail.com>
2018-12-02 18:19:52 +0300

    Modified settings for temlate dir

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'qa',
     'ask',
+    'autofixture',
 ]
 
 MIDDLEWARE = [
@@ -53,10 +54,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ask.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
