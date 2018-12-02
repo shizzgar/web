16b1af8c5ea460e80e30c7b377b78837bd3963d7
shizzgar <steeji.rat@gmail.com>
2018-12-02 18:20:34 +0300

    some fixes in model

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class QuestionManager(models.Manager):
@@ -15,15 +15,15 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    likes = models.ManyToManyField('auth.User', related_name='likes_set')
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes_set')
     objects = QuestionManager()
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
