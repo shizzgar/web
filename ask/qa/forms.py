8e3dc22db96118976fc11c73263e1a44eadd752b
shizzgar <steeji.rat@gmail.com>
2018-12-05 14:55:19 +0300

    added ask and answer fom for ask.html and question.html

---
+++ ask/qa/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from . import models
+
+
+class AskForm(forms.Form):
+    title = forms.CharField()
+    text = forms.CharField(widget=forms.Textarea)
+    
+
+    def clean(self):
+        return  self.cleaned_data
+
+
+    def save(self):
+        question = models.Question(**self.cleaned_data)
+        question.author_id = 1
+        question.save()
+        return question
+
+
+class AnswerForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    question_id = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+
+    def clean(self):
+        return self.cleaned_data
+
+
+    def save(self, id):
+        answer = models.Answer(**self.cleaned_data)
+        answer.author_id = 1
+        answer.question_id = id
+        answer.save()
+        return answer
