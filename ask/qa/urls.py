9d9de920e24eae047156dc9460596a755772c1ad
shizzgar <steeji.rat@gmail.com>
2018-12-15 20:30:38 +0300

    some fixes

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.q_new, name='q_new'),
-    url(r'^login/$', views.test, name='test'),
-    url(r'^signup/$', views.test, name='test'),
+    url(r'^login/$', views.logining, name='logining'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^ask/$', views.q_add, name='q_add'),
     url(r'^popular/$', views.q_popular, name='q_popular'),
     url(r'^new/$', views.test, name='test'),
